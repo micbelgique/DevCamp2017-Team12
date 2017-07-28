6d5b1c6486e7be5d2201e307f22c6e63fbc69101
David <david_g@live.be>
2017-07-28 19:16:34 +0200

    Clean code

---
+++ grial-uikit-extended-1.5.2/samples/Grial/Grial/Models/Personne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace UXDivers.Artina.Grial
+{
+	public class Personne
+	{
+		public string nom{ get; set; }
+		public string prenom{ get; set; }
+		public DateTime dateNaissance{ get; set; }
+		public List<Vehicule> vehicules { get; set; }
+
+		public Personne()
+		{
+			vehicules = new List<Vehicule>();
+		}
+	}
+}

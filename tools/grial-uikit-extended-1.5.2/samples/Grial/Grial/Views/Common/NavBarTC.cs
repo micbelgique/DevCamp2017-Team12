6633c702bfe34d9322e290589af1e59ea245390b
Denis Omelcenco <denis@skygate.be>
2017-07-29 13:35:31 +0200

    iOS device integration, Navbar integration

---
+++ tools/grial-uikit-extended-1.5.2/samples/Grial/Grial/Views/Common/NavBarTC.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace UXDivers.Artina.Grial
+{
+	public partial class NavBarTC : ContentView
+	{
+		public NavBarTC()
+		{
+			InitializeComponent();
+
+		}
+		public void ChangeLeftLabel(string text, bool visible)
+		{
+			this.LeftLabel.Text = text;
+			this.LeftLabel.IsVisible = visible;
+		}
+		public void ChangeCenterLabel(string text, bool visible)
+		{
+			this.CenterLabel.Text = text;
+			this.CenterLabel.IsVisible = visible;
+		}
+
+		public void OnHamburgerIconTapped(Object sender, EventArgs e)
+		{
+			Element current = this;
+
+			while (current.Parent != null)
+			{
+				current = current.Parent;
+				if (current.GetType().Name == "RootPage")
+				{
+					break;
+				}
+			}
+
+			var master = current as MasterDetailPage;
+
+			if (master != null)
+			{
+				master.IsPresented = true;
+			}
+		}
+
+		public async void OnLeftTapped(Object sender, EventArgs e)
+		{
+		//	await Navigation.PopAsync();
+		}
+		public async void OnRightTapped(Object sender, EventArgs e)
+		{
+		//	await Navigation.PushAsync(new PharmacyPage());
+		}
+	}
+}
+

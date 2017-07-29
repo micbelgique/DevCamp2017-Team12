a8e5a3767f09cdcc226748873e56001f984131fd
ekBilal <elkhattouti.b@gmail.com>
2017-07-29 11:11:38 +0200

    DEmandeInfo

---
+++ grial-uikit-extended-1.5.2/samples/Grial/Grial/Models/DemandeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UXDivers.Artina.Grial
+{
+	public class DemandeInfo
+	{
+		public AutoritesPublique autorite { get; set; }
+		public string raison { get; set; }
+
+		public DemandeInfo(AutoritesPublique autorite, string raison)
+		{
+			this.autorite = autorite;
+			this.raison = raison;
+		}
+	}
+}

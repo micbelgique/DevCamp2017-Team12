7be680e672179550453ed3459c0980809d853536
ekBilal <elkhattouti.b@gmail.com>
2017-07-29 12:11:34 +0200

    Véhicule

--- grial-uikit-extended-1.5.2/samples/Grial/Grial/Data/Data.cs
+++ grial-uikit-extended-1.5.2/samples/Grial/Grial/Data/Data.cs
@@ -5,22 +5,8 @@ namespace UXDivers.Artina.Grial
 	{
 		public Data()
 		{
-			Vehicule maVoiture = new Vehicule();
-			maVoiture.vehiculeID = 1;
-			maVoiture.numImmatriculation = "1-ERR-404";
-			maVoiture.datePremiereImmatriculation = new DateTime(1921, 09, 1);
-			maVoiture.constructeur = "Ford";
-			maVoiture.cylindree = 8;
-			maVoiture.puissanceMaximale = 589;
-			maVoiture.typeDeCarburant = "essances";
-			maVoiture.annéeDeConstruction = new DateTime(1920, 1, 1);
-
 			Personne moi = new Personne();
-			moi.nom = "Omelcenco";
-			moi.prenom = "Denis";
-			moi.dateNaissance = new DateTime(1990, 12, 9);
-
-			moi.vehicules.Add(maVoiture);
+			Vehicule maVoiture = new Vehicule(moi);
 
 			AutoritesPublique police = new AutoritesPublique();
 			police.nom = "Police";
--- grial-uikit-extended-1.5.2/samples/Grial/Grial/Models/Vehicule.cs
+++ grial-uikit-extended-1.5.2/samples/Grial/Grial/Models/Vehicule.cs
@@ -13,6 +13,42 @@ namespace UXDivers.Artina.Grial
 		public DateTime annéeDeConstruction { get; set; }
 		public Personne proprio { get; set; }
 
+
+		Random rand;
+
+		public Vehicule(Personne proprio)
+		{
+			rand = new Random();
+
+			if (proprio == null) throw new ArgumentException("Le vehicule doit avoir un propriétaire");
+
+			this.proprio = proprio;
+
+			vehiculeID = rand.Next(100);
+			numImmatriculation = getImmatriculation();
+			//une date random entre 1900 et cette année
+			datePremiereImmatriculation = new DateTime(rand.Next(1900,DateTime.Today.Year));
+			constructeur = "Ford";
+			cylindree = rand.Next(10, 80) / 10;
+			puissanceMaximale = rand.Next(100, 1001);
+			typeDeCarburant = "essance";
+			annéeDeConstruction = datePremiereImmatriculation;
+
+			this.proprio.vehicules.Add(this);
+		}
+
+		String getImmatriculation()
+		{
+			string immatriculation = "1";
+			for (int i = 0; i< 3; i++)
+			{
+				char c = Convert.ToChar(rand.Next(65, 90));
+				immatriculation += c;
+			}
+			for (int i = 0; i< 3; i++) immatriculation += rand.Next(0,9);
+			return immatriculation;
+		}
+
 		public Personne consulterProprio(DemandeInfo demande)
 		{
 			proprio.consulter(demande);
